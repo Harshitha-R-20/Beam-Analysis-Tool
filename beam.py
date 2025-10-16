31104a4152d32ef97071cb009eb906fbdb426e39
Harshitha-R-20 <harshitha21pc021@gmail.com>
2025-10-17 01:36:14 +0530

    Improved plot readability in beam.py: larger fonts, axis units, legends, optional PNG/PDF export for diagrams. Ready for educational and publication use.

--- beam.py
+++ beam.py
@@ -40,41 +40,49 @@ class Beam:
         return M
 
     def deflection(self, x, n=1000):
-        # Calculate deflection using double integration of M/EI (assumes simply supported beam)
         xs = np.linspace(0, self.length, n)
         M_vals = np.array([self.bending_moment(xi) for xi in xs])
         curvature = M_vals / (self.E * self.I)
         dx = xs[1] - xs[0]
         slope = np.cumsum(curvature) * dx
         defl = np.cumsum(slope) * dx
-        # Interpolate deflection at x
         return np.interp(x, xs, defl)
 
-    def plot_diagrams(self):
+    def plot_diagrams(self, export=False):
         xs = np.linspace(0, self.length, 1000)
         V = np.array([self.shear_force(x) for x in xs])
         M = np.array([self.bending_moment(x) for x in xs])
         D = np.array([self.deflection(x) for x in xs])
 
-        fig, axs = plt.subplots(3, 1, figsize=(10, 12))
+        fig, axs = plt.subplots(3, 1, figsize=(12, 14))
 
-        axs[0].plot(xs, V, label='Shear Force (kN)')
-        axs[0].set_title('Shear Force Diagram')
-        axs[0].set_xlabel('Beam Length (m)')
-        axs[0].set_ylabel('Shear Force (kN)')
+        font_title = {'fontsize': 16, 'fontweight': 'bold'}
+        font_axis = {'fontsize': 13}
+        font_label = {'fontsize': 12}
+
+        axs[0].plot(xs, V, label='Shear Force', color='navy')
+        axs[0].set_title('Shear Force Diagram', **font_title)
+        axs[0].set_xlabel('Beam Length (m)', **font_axis)
+        axs[0].set_ylabel('Shear Force (kN)', **font_label)
+        axs[0].legend(loc='best')
         axs[0].grid(True)
 
-        axs[1].plot(xs, M, label='Bending Moment (kNm)', color='orange')
-        axs[1].set_title('Bending Moment Diagram')
-        axs[1].set_xlabel('Beam Length (m)')
-        axs[1].set_ylabel('Bending Moment (kNm)')
+        axs[1].plot(xs, M, label='Bending Moment', color='orange')
+        axs[1].set_title('Bending Moment Diagram', **font_title)
+        axs[1].set_xlabel('Beam Length (m)', **font_axis)
+        axs[1].set_ylabel('Bending Moment (kNm)', **font_label)
+        axs[1].legend(loc='best')
         axs[1].grid(True)
 
-        axs[2].plot(xs, D, label='Deflection (m)', color='green')
-        axs[2].set_title('Deflection Diagram')
-        axs[2].set_xlabel('Beam Length (m)')
-        axs[2].set_ylabel('Deflection (m)')
+        axs[2].plot(xs, D, label='Deflection', color='green')
+        axs[2].set_title('Deflection Diagram', **font_title)
+        axs[2].set_xlabel('Beam Length (m)', **font_axis)
+        axs[2].set_ylabel('Deflection (m)', **font_label)
+        axs[2].legend(loc='best')
         axs[2].grid(True)
 
         plt.tight_layout()
+        if export:
+            fig.savefig('beam_diagrams.png')
+            fig.savefig('beam_diagrams.pdf')
         plt.show()
